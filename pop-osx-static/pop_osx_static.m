b3b4d714b50c9be43ad1512105c414b20e76a457
Jason Brennan <i.jasonbrennan@gmail.com>
2015-07-30 16:53:17 -0400

    Adds static library target for OS X.

---
+++ pop-osx-static/pop_osx_static.m
@@ -0,0 +1,13 @@
+//
+//  pop_osx_static.m
+//  pop-osx-static
+//
+//  Created by Jason Brennan on 2015-07-30.
+//  Copyright (c) 2015 Facebook. All rights reserved.
+//
+
+#import "pop_osx_static.h"
+
+@implementation pop_osx_static
+
+@end
